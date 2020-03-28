f906c1545da3b8f8453f4493a667ebef05362ba2
Zsolt Krüpl <hg2ecz@ham.hu>
2020-03-28 08:51:16 +0100

    Some modifications

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 use std::env;
-use std::io::Read;
 use std::fs::File;
+use std::io::Read;
 
-/// Opcodes determined by the lexer
-#[derive(Debug)]
 #[derive(Clone)]
 enum OpCode {
     IncrementPointer,
@@ -16,7 +14,24 @@ enum OpCode {
     LoopEnd,
 }
 
-#[derive(Debug)]
+fn char_to_opcode(source: String) -> Vec<OpCode> {
+    let mut op = Vec::new();
+    for ch in source.chars() {
+        match ch {
+            '>' => op.push(OpCode::IncrementPointer),
+            '<' => op.push(OpCode::DecrementPointer),
+            '+' => op.push(OpCode::Increment),
+            '-' => op.push(OpCode::Decrement),
+            '.' => op.push(OpCode::Write),
+            ',' => op.push(OpCode::Read),
+            '[' => op.push(OpCode::LoopBegin),
+            ']' => op.push(OpCode::LoopEnd),
+            _ => (),
+        };
+    }
+    op
+}
+
 #[derive(Clone)]
 enum Instruction {
     IncrementPointer,
@@ -25,90 +40,60 @@ enum Instruction {
     Decrement,
     Write,
     Read,
-    Loop(Vec<Instruction>)
+    Loop(Vec<Instruction>),
 }
 
-/// Lexer turns the source code into a sequence of opcodes
-fn lex(source: String) -> Vec<OpCode> {
-    let mut operations = Vec::new();
-
-    for symbol in source.chars() {
-        let op = match symbol {
-            '>' => Some(OpCode::IncrementPointer),
-            '<' => Some(OpCode::DecrementPointer),
-            '+' => Some(OpCode::Increment),
-            '-' => Some(OpCode::Decrement),
-            '.' => Some(OpCode::Write),
-            ',' => Some(OpCode::Read),
-            '[' => Some(OpCode::LoopBegin),
-            ']' => Some(OpCode::LoopEnd),
-            _ => None
-        };
-
-        // Non-opcode characters are simply comments
-        match op {
-            Some(op) => operations.push(op),
-            None => ()
-        }
-    }
-
-    operations
-}
-
-fn parse(opcodes: Vec<OpCode>) -> Vec<Instruction> {
-    let mut program: Vec<Instruction> = Vec::new();
+fn parse_loops(opcodes: Vec<OpCode>) -> Vec<Instruction> {
+    let mut prog: Vec<Instruction> = Vec::new();
     let mut loop_stack = 0;
-    let mut loop_start = 0;
+    let mut loop_begin = 0;
 
     for (i, op) in opcodes.iter().enumerate() {
         if loop_stack == 0 {
-            let instr = match op {
-                OpCode::IncrementPointer => Some(Instruction::IncrementPointer),
-                OpCode::DecrementPointer => Some(Instruction::DecrementPointer),
-                OpCode::Increment => Some(Instruction::Increment),
-                OpCode::Decrement => Some(Instruction::Decrement),
-                OpCode::Write => Some(Instruction::Write),
-                OpCode::Read => Some(Instruction::Read),
+            match op {
+                OpCode::IncrementPointer => prog.push(Instruction::IncrementPointer),
+                OpCode::DecrementPointer => prog.push(Instruction::DecrementPointer),
+                OpCode::Increment => prog.push(Instruction::Increment),
+                OpCode::Decrement => prog.push(Instruction::Decrement),
+                OpCode::Write => prog.push(Instruction::Write),
+                OpCode::Read => prog.push(Instruction::Read),
 
                 OpCode::LoopBegin => {
-                    loop_start = i;
+                    loop_begin = i;
                     loop_stack += 1;
-                    None
-                },
+                }
 
                 OpCode::LoopEnd => panic!("loop ending at #{} has no beginning", i),
             };
-
-            match instr {
-                Some(instr) => program.push(instr),
-                None => ()
-            }
         } else {
             match op {
                 OpCode::LoopBegin => {
                     loop_stack += 1;
-                },
+                }
                 OpCode::LoopEnd => {
                     loop_stack -= 1;
 
                     if loop_stack == 0 {
-                        program.push(Instruction::Loop(parse(opcodes[loop_start+1..i].to_vec())));
+                        let instr_vec = parse_loops(opcodes[loop_begin + 1..i].to_vec());
+                        prog.push(Instruction::Loop(instr_vec));
                     }
-                },
+                }
                 _ => (),
             }
         }
     }
 
     if loop_stack != 0 {
-        panic!("loop that starts at #{} has no matching ending!", loop_start);
+        panic!(
+            "loop that starts at #{} has no matching ending!",
+            loop_begin
+        );
     }
 
-    program
+    prog
 }
 
-/// Executes a program that was previously parsed
-fn run(instructions: &Vec<Instruction>, tape: &mut Vec<u8>, data_pointer: &mut usize) {
+fn run_program(instructions: &[Instruction], tape: &mut Vec<u8>, data_pointer: &mut usize) {
     for instr in instructions {
         match instr {
             Instruction::IncrementPointer => *data_pointer += 1,
@@ -118,12 +103,14 @@ fn run(instructions: &Vec<Instruction>, tape: &mut Vec<u8>, data_pointer: &mut u
             Instruction::Write => print!("{}", tape[*data_pointer] as char),
             Instruction::Read => {
                 let mut input: [u8; 1] = [0; 1];
-                std::io::stdin().read_exact(&mut input).expect("failed to read stdin");
+                std::io::stdin()
+                    .read_exact(&mut input)
+                    .expect("failed to read stdin");
                 tape[*data_pointer] = input[0];
-            },
+            }
             Instruction::Loop(nested_instructions) => {
                 while tape[*data_pointer] != 0 {
-                    run(&nested_instructions, tape, data_pointer)
+                    run_program(&nested_instructions, tape, data_pointer)
                 }
             }
         }
@@ -131,30 +118,21 @@ fn run(instructions: &Vec<Instruction>, tape: &mut Vec<u8>, data_pointer: &mut u
 }
 
 fn main() {
-    // Determine which file to execute
     let args: Vec<String> = env::args().collect();
-
     if args.len() != 2 {
         println!("usage: bf <file.bf>");
         std::process::exit(1);
     }
-    
-    let filename = &args[1];
+    let mut file = File::open(&args[1]).expect("program file not found");
 
-    // Read file
-    let mut file = File::open(filename).expect("program file not found");
     let mut source = String::new();
-    file.read_to_string(&mut source).expect("failed to read program file");
+    file.read_to_string(&mut source)
+        .expect("failed to read program file");
 
-    // Lex file into opcodes
-    let opcodes = lex(source);
+    let opcodes = char_to_opcode(source);
+    let program = parse_loops(opcodes);
 
-    // Parse opcodes into program
-    let program = parse(opcodes);
-
-    // Set up environment and run program
     let mut tape: Vec<u8> = vec![0; 1024];
     let mut data_pointer = 512;
-
-    run(&program, &mut tape, &mut data_pointer);
+    run_program(&program, &mut tape, &mut data_pointer);
 }
